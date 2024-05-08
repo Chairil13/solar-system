3e71a0350a703af053207b1799cb6cae0e85cdf4
Chairil Ali <mrnightmare13081999@gmail.com>
2024-05-08 23:15:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// 🌞 🌍 🌕 🪐 🌠 🌌
+// Are you looking for something? Nothing to see here!
+// Pen by @DeyJordan
+
+// Twitter:   https://twitter.com/DeyJordan
+// Linkedin:  https://www.linkedin.com/in/dey-jordan/
